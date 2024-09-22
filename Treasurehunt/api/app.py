14131e535ed12b88a99712d0a5d841cec1c35a67
Jaroncoder <144526969+Jaroncoder@users.noreply.github.com>
2024-09-22 12:02:07 +0530

    Files for Treasure hunt

---
+++ Treasurehunt/api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+CORS(app)
+
+client = MongoClient('mongodb+srv://sample_user:sample123@cluster0.xig4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client.user_database
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    credentials_collection = db.credentials.find_one({'username': email})
+
+    if credentials_collection and bcrypt.checkpw(password.encode('utf-8'), credentials_collection['password']):
+        return jsonify({"message": "Login successful!"}), 200
+    else:
+        return jsonify({"message": "Invalid credentials."}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Treasurehunt/frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import LoginPage from './LoginPage'; // Change this line
+
+function App() {
+    return (
+        <div className="App">
+            <LoginPage />
+        </div>
+    );
+}
+
+export default App;
